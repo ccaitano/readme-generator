9f0f9073272359d1022b19aaf96a54df32601a8b
ccaitano <cheryl.caitano@gmail.com>
2022-07-29 19:45:38 -1000

    Add all questions requiring user input

--- index.js
+++ index.js
@@ -3,15 +3,62 @@
 const fs = require("fs");
 //Asks user for data
 const inquirer = require("inquirer");
-//Generate HTML file
+//Generate markdown file
 const generateMd = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: "input",
-        message: "Welcome to the README File Generator",
+        message: "Welcome to the README File Generator. Please press 'ENTER' to begin.",
         name: "welcome"
+    },
+    {
+        type:"input",
+        message: "Please enter your project title: ",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Please enter your project description: ",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Please enter your installation instructions: ",
+        name: "installins"
+    },
+    {
+        type: "input",
+        message: "Please enter your usage information: ",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Please enter your contribution guidelines: ",
+        name: "contributions"
+    },
+    {
+        type: "input",
+        message: "Please enter your test instructions: ",
+        name: "testins"
+    },
+    {
+        type: "list",
+        message: "Please choose a liceense applicable to your project.",
+        name: "license",
+        choices: ["MIT","None"],
+        default: "None"
+    },
+    {
+        type: "input",
+        message: "Please enter your GitHub username: ",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "Please enter your e-mail address: ",
+        name: "email"
     }
 ];
 
@@ -24,8 +71,18 @@ function writeToFile(fileName, data) {
 // TODO: Create a function to initialize app
 function init() {}
     //Ask the user the questions
-
-        //THEN I want to pass responses to generageMarkdown so we can get the markdown
+    // for (const question of questions) {
+    //     inquirer
+    //         .prompt([
+    //             {
+    //                 question
+    //             },
+    //         ])
+    //         .then(answers => {
+    //             console.log(question[2]);
+    //         });
+    // }
+        //THEN I want to pass responses to generateMarkdown so we can get the markdown
 
         //THEN write the 'markdown' to a file like 'OUTPUT_README.md'
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,7 +10,7 @@ function renderLicenseLink(license) {}
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
-function renderTItle(title) {
+function renderTitle(title) {
   return `# ${data.title}`;
 }
 // TODO: Create a function to generate markdown for README
