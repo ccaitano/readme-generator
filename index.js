841c543f444f545be113982172fa2c6765c2b0bd
ccaitano <cheryl.caitano@gmail.com>
2022-08-02 10:50:49 -1000

    Update template literals for formatting in final README file

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const questions = [
     },
     {
         type:"input",
-        message: "Please enter your project title: ",
+        message: "Please enter your project title (Note: Must match GitHub project title for deployed links to function correctly): ",
         name: "title"
     },
     {
@@ -45,9 +45,9 @@ const questions = [
     },
     {
         type: "list",
-        message: "Please choose a liceense applicable to your project.",
+        message: "Please choose a license applicable to your project.",
         name: "license",
-        choices: ["Apache 2.0","MIT","None"],
+        choices: ["Apache 2.0","MIT","None Applicable"],
         default: "None"
     },
     {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,99 +4,76 @@ let readmeBadge = "";
 let readmeLink = "";
 let readmeLicense = "";
 
-function renderLicenseBadge(license) {
-  const apacheBadge = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-  const mitBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-  const noBadge = "";
+function renderLicense(license) {
+  const apacheLicense = {
+    badge: "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]",
+    link: "https://opensource.org/licenses/Apache-2.0",
+    wording: `
+[Apache 2.0 License](https://opensource.org/licenses/Apache-2.0)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+    `    
+  }
+  const mitLicense = {
+    badge: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]",
+    link: "https://opensource.org/licenses/MIT",
+    wording: `
+[MIT License](https://opensource.org/licenses/MIT)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+        
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+          
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+      `
+  }
+  const noLicense = {
+    badge: "",
+    link: "",
+    wording: "No Applicable License"
+  }
   switch (license) {
     default:
-      readmeBadge = noBadge;
+      readmeBadge = noLicense.badge;
+      readmeLink = noLicense.link;
+      readmeLicense = noLicense.wording;
       break;
     case "Apache 2.0":
-      readmeBadge = apacheBadge;
+      readmeBadge = apacheLicense.badge;
+      readmeLink = apacheLicense.link;
+      readmeLicense = apacheLicense.wording;
       break;
     case "MIT":
-      readmeBadge = mitBadge;
+      readmeBadge = mitLicense.badge;
+      readmeLink = mitLicense.link;
+      readmeLicense = mitLicense.wording;
       break;
     case "None":
-      readmeBadge = noBadge;
+      readmeBadge = noLicense.badge;
+      readmeLink = noLicense.link;
+      readmeLicense = noLicense.wording;
       break;  
   }
-  return readmeBadge;
+  return readmeBadge, readmeLink, readmeLicense;
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  const apacheLink = "https://opensource.org/licenses/Apache-2.0";
-  const mitLink = "https://opensource.org/licenses/MIT)";
-  const noLink = "";
-  switch (license) {
-    default:
-      readmeLink = noLink;
-      break;
-    case "Apache 2.0":
-      readmeLink = apacheLink;
-      break;
-    case "MIT":
-      readmeLink = mitLink;
-      break;
-    case "None":
-      readmeLink = noLink;
-      break;  
-  }
-  return readmeLink;
-}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  const apacheLicense = `
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-  `;
-  const mitLicense = `
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-    
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-      
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-  `;
-  const noLicense = "";
-  switch (license) {
-    default:
-      readmeLicense = noLicense;
-      break;
-    case "Apache 2.0":
-      readmeLicense = apacheLicense;
-      break;
-    case "MIT":
-      readmeLicense = mitLicense;
-      break;
-    case "None":
-      readmeLicense = noLicense;
-      break;  
-  }
-  return readmeLicense;
-}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   const license = data.license;
-  renderLicenseBadge(license);
-  renderLicenseLink(license);
-  renderLicenseSection(license);
+  renderLicense(license);
     readMeString = `
-# ${data.title} - ${readmeBadge}
+# ${data.title} - ${readmeBadge}(${readmeLink})
     
 ➡️ [DEPLOYED APPLICATION](https://${data.username}.github.io/${data.title}/)
     
@@ -124,19 +101,17 @@ Visit the [deployed site](https://${data.username}.github.io/${data.title}/), th
 ${data.usage}
     
 ## **License**
-
-[${data.license} License](${readmeLink})
     
 Copyright &copy;2022 - ${data.username}
   
 <p>
-
-${readmeLicense}
-    
+${readmeLicense}  
 </p>
     
 ## **Contributions**
-    
+
+When contributing to this repository, please first discuss the change you wish to make via opening an issue or e-mail the owners of this repository. Specific instructions are included below:
+
 ${data.contributions}
 
 ## **Test**
@@ -150,7 +125,6 @@ For any questions or contributions please contact me on Github or by e-mail:
 [Github Profile](https://www.github.com/${data.username})  
 [Email Me](mailto:${data.email})
     `
-
   return readMeString;
 }
 
